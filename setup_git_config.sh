9a8a15197fc3f125322f7d220f4d731ff1b4f059
Krzysztof Ruczkowski <krzysztof.ruczkowski@nokia.com>
2019-08-06 15:10:48 +0200

    setup_git_config.sh - Added checking for unpushed submodules

--- setup_git_config.sh
+++ setup_git_config.sh
@@ -8,3 +8,6 @@ git config --global diff.submodule log
 
 # Rebase by default in `git pull` (without destroying the branch structure)
 git config --global pull.rebase merges
+
+# Abort pushing if any of the submodules is not pushed
+git config --global push.recurseSubmodules check
