904b689cc7ea829be06a9cbbec6f8e9dbe5b1f93
Krzysztof Ruczkowski <krzysztof.ruczkowski@nokia.com>
2019-08-08 13:50:35 +0200

    bash scripts refactored

--- install_wallpaper.sh
+++ install_wallpaper.sh
@@ -7,7 +7,7 @@ f () {
     exit $errcode
 }
 trap f ERR
-mkdir -p $HOME/Pictures
-cd $HOME/Pictures
+mkdir -p "$HOME"/Pictures
+cd "$HOME"/Pictures || exit 1
 wget https://d34urnl45u363e.cloudfront.net/store/mediaobject/6883/image/large-a7f5a64d0132562636a95e9d664c8ef8.jpg -O betterlockscreen_hex_wallpaper.jpg
 betterlockscreen -u betterlockscreen_hex_wallpaper.jpg
--- pkglist/install_packages.sh
+++ pkglist/install_packages.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 if [[ -z $1  ]]; then
   echo "Argument needed."
 else
-  yay -S --needed --noconfirm - < $1
+  yay -S --needed --noconfirm - < "$1"
 fi
 
