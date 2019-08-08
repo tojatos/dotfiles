74a6b9220718b3b428911e171bdb1514545ed71c
Krzysztof Ruczkowski <krzysztof.ruczkowski@nokia.com>
2019-08-08 13:52:01 +0200

    unnecessary trap removed

--- install_wallpaper.sh
+++ install_wallpaper.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-f () {
-    errcode=$?
-	echo "failure:"
-    echo "$BASH_COMMAND"
-    echo "on line ${BASH_LINENO[0]}"
-    exit $errcode
-}
-trap f ERR
 mkdir -p "$HOME"/Pictures
 cd "$HOME"/Pictures || exit 1
 wget https://d34urnl45u363e.cloudfront.net/store/mediaobject/6883/image/large-a7f5a64d0132562636a95e9d664c8ef8.jpg -O betterlockscreen_hex_wallpaper.jpg
