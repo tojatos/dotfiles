f6310c387fab767805bfccfb1a5720d42760bdcd
Krzysztof Ruczkowski <krzysztof.ruczkowski@nokia.com>
2019-09-02 10:00:57 +0200

    setup_git_config.sh - set pull.rebase in older git versions correctly

--- setup_git_config.sh
+++ setup_git_config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
+git_version=$(git --version | awk '{ print $3 }')
+pull_rebase_merges_minimal_version=2.0.0 # TODO: select a correct version, this is good enough for now
+
 # Display submodule changes in `git status`
 git config --global status.submoduleSummary true
 
@@ -7,7 +12,11 @@ git config --global status.submoduleSummary true
 git config --global diff.submodule log
 
 # Rebase by default in `git pull` (without destroying the branch structure)
-git config --global pull.rebase merges
+if version_gt "${git_version}" "${pull_rebase_merges_minimal_version}" ; then
+  git config --global pull.rebase merges
+else
+  git config --global pull.rebase true
+fi
 
 # Abort pushing if any of the submodules is not pushed
 git config --global push.recurseSubmodules check
