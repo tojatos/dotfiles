8eadc8cfd12d216e5536df257de0c484ba673aa2
Krzysztof Ruczkowski <krzysztof.ruczkowski@nokia.com>
2019-10-18 14:03:58 +0200

    bootstrap.sh - custom installations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -e
 for file in install/*; do
   ${file}
+done
+
+CUSTOM_INSTALLATIONS=(st dmenu)
+
+for CUSTOM_INSTALLATION in "${CUSTOM_INSTALLATIONS[@]}"; do
+  pushd "$CUSTOM_INSTALLATION"
+    install.sh
+  popd "$CUSTOM_INSTALLATION"
 done
 
 for file in setup/*; do
