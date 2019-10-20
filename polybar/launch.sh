9903a8d4938cd2c19f85cb4bf7a068f7805b13c0
Krzysztof Ruczkowski <tojatos@gmail.com>
2019-10-20 13:44:32 +0200

    polybar - generic network and battery interfaces

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Export defaults
+export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+export DEFAULT_BATTERY=$(find /sys/class/power_supply/ -type l | xargs basename | head -n1)
+
 # Launch bar
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload bar &
