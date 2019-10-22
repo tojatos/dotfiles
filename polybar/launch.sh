060a2859502d0dc8a3b017ef24784b54089f688b
tojatos <tojatos@gmail.com>
2019-10-22 21:44:09 +0200

    polybar - changed command for default network interface
    it is used now to get default WIFI network interface

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Export defaults
-export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+export DEFAULT_NETWORK_INTERFACE=$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')
 export DEFAULT_BATTERY=$(find /sys/class/power_supply/ -type l | xargs basename | head -n1)
 
 # Launch bar
