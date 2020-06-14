2ac98cec96b62205dc2b9622463fb47b1b1d7e5f
tojatos <tojatos@gmail.com>
2020-06-14 12:57:39 +0200

    wallpaper installation script refactored

--- install/wallpaper.sh
+++ install/wallpaper.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
-
+#!/bin/bash -e
 WALLPAPER_URL="https://d34urnl45u363e.cloudfront.net/store/mediaobject/6883/image/large-a7f5a64d0132562636a95e9d664c8ef8.jpg"
+TARGET_FILENAME="betterlockscreen_wallpaper.jpg"
 
 mkdir -p "$HOME"/Pictures
 cd "$HOME"/Pictures || exit 1
 
-wget "$WALLPAPER_URL" -O betterlockscreen_wallpaper.jpg -q --show-progress
-betterlockscreen -u betterlockscreen_wallpaper.jpg
+if [[ -f "$TARGET_FILENAME" ]]; then
+  echo "wallpaper is already installed, skipping"
+  exit 0
+fi
+
+wget "$WALLPAPER_URL" -O "$TARGET_FILENAME" -q --show-progress
+echo "setting wallpaper on the lock screen..."
+betterlockscreen -u "$TARGET_FILENAME" >/dev/null
+echo "wallpaper installed successfully"
