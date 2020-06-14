990c56ecf552fe3a04bd5f22c2f7e22418214310
tojatos <tojatos@gmail.com>
2020-06-14 12:58:22 +0200

    bootstrap script updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,12 +20,21 @@ for file in install/*.sh; do
   ${file}
 done
 
-for CUSTOM_INSTALLATION in install/custom/*; do
-  info "Installing $CUSTOM_INSTALLATION"
-  cd "$CUSTOM_INSTALLATION"
-    ./install.sh
-  cd -
+cd install/custom
+
+for CUSTOM_INSTALLATION in *; do
+    info "Installing $CUSTOM_INSTALLATION"
+  if pacman -Qs "${CUSTOM_INSTALLATION}_custom" &>/dev/null; then
+    echo "$CUSTOM_INSTALLATION is already installed, skipping"
+  else
+
+    cd "$CUSTOM_INSTALLATION"
+      ./install.sh
+    cd -
+  fi
 done
+
+cd "$BASE_DIR"
 
 for file in setup/*; do
   info "Setting up $(basicname "${file}")"
