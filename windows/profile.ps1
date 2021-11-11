4e4c8b1a8d14a499d651cc93388b7254646cf8ad
tojatos <tojatos@gmail.com>
2021-11-11 04:34:47 +0100

    windows dotfiles updated

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -50,12 +50,29 @@ function dtags { git log --tags --simplify-by-decoration --pretty="format:%ai %d
 function gdm { git diff master@{1} master }
 function a { .venv/Scripts/activate }
 
-function pwr { Set-Location E:\Documents\PWR6 }
+function pwr { Set-Location E:\Documents\PWR7 }
 function d { Set-Location E:\Documents }
 function work { Set-Location D:\Scripts }
 function ipa { Get-NetIPAddress | where AddressFamily -eq IPv4 | select InterfaceAlias,IPAddress | Format-Table }
 function ssh-copy-id { cat ~/.ssh/id_rsa.pub | ssh $args "cat >> ~/.ssh/authorized_keys" }
 function to_mp4 { ffmpeg -i $args "$([io.path]::GetFileNameWithoutExtension($args)).mp4" }
+function refresh_path { $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") }
+function edit_profile { vim ~\.dotfiles\windows\profile.ps1 }
+
+# https://stackoverflow.com/a/5501909/7136056
+function Reload-Profile {
+    @(
+        $Profile.AllUsersAllHosts,
+        $Profile.AllUsersCurrentHost,
+        $Profile.CurrentUserAllHosts,
+        $Profile.CurrentUserCurrentHost
+    ) | % {
+        if(Test-Path $_){
+            Write-Verbose "Running $_"
+            . $_
+        }
+    }
+}
 
 # PowerShell parameter completion shim for the dotnet CLI
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
