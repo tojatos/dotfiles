d0ec26e168fe01cf35f9472b80c547ff9aa2ad08
tojatos <tojatos@gmail.com>
2022-01-05 23:26:48 +0100

    windows link settings and correct terminal path

--- windows/link.ps1
+++ windows/link.ps1
@@ -1,11 +1,15 @@
 Import-Module $PSScriptRoot\utils.psm1 -Force
 $startup_dir = realpath "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-$powershell_dir = realpath "E:\Documents\PowerShell"
+$powershell_dir = realpath $(Split-Path -parent $profile)
+$wt_settings_dir = realpath "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+
+rm "$wt_settings_dir\settings.json"
 
 $links = @{
     "profile.ps1"="$powershell_dir";
     "utils.psm1"="$powershell_dir";
     "main.ahk"="$startup_dir";
+    "settings.json"="$wt_settings_dir";
 }
 
 foreach ($l in $links.GetEnumerator()) {
