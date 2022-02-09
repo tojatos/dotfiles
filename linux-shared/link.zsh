6f52619567e01f7b68acb481f07e525cac849b98
tojatos <tojatos@gmail.com>
2022-02-09 17:16:56 +0100

    zsh scripts added; mac init

---
+++ linux-shared/link.zsh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+BASE_DIR=$(cd .. && pwd)
+
+enable_module () {
+  source "$BASE_DIR/lib/$1"
+}
+
+enable_module logging
+
+declare -A links
+links=(
+  [".bashrc"]="$HOME"
+  [".zshrc"]="$HOME"
+  [".aliases"]="$HOME"
+  [".ideavimrc"]="$HOME"
+  [".p10k.zsh"]="$HOME"
+  [".tmux.conf"]="$HOME"
+  ["procps"]="$HOME/.config"
+  ["QtCreator.ini"]="$HOME/.config/QtProject"
+)
+
+basicname () {
+  local file
+  file="$(basename "$1")"
+  echo "${file/%.*}"
+}
+
+for file in "${(@k)links}"; do
+  target=${links[$file]}
+  filepath="$(greadlink -f "$file")"
+  info "Linking $file to $target"
+  mkdir -p "$target"
+  ln -sf "$filepath" "$target"
+  (( $? != 0 )) && warn "Error linking $file"
+done
---
+++ shared/link.zsh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BASE_DIR=$(cd .. && pwd)
+
+enable_module () {
+  source "$BASE_DIR/lib/$1"
+}
+
+enable_module logging
+
+declare -A links
+links=(
+  [".vimrc"]="$HOME"
+  [".vim"]="$HOME"
+  [".ideavimrc"]="$HOME"
+  [".vim/plugged/vimpager/vimcat"]="$HOME/.bin"
+  [".vim/plugged/vimpager/vimpager"]="$HOME/.bin"
+  [".ssh/config"]="$HOME/.ssh"
+)
+
+basicname () {
+  local file
+  file="$(basename "$1")"
+  echo "${file/%.*}"
+}
+
+for file in "${(@k)links}"; do
+  target=${links[$file]}
+  filepath="$(greadlink -f "$file")"
+  info "Linking $file to $target"
+  mkdir -p "$target"
+  ln -sf "$filepath" "$target"
+  (( $? != 0 )) && warn "Error linking $file"
+done
