c177ae333b4afc9750c3533ad033010417569ac9
tojatos <tojatos@gmail.com>
2022-10-11 16:20:50 +0200

    wifip added

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -63,6 +63,18 @@ function to_mp4 { ffmpeg -i $args "$([io.path]::GetFileNameWithoutExtension($arg
 function refresh_path { $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") }
 function edit_profile { vim ~\.dotfiles\windows\profile.ps1 }
 
+function wifip {
+  $profiles=(netsh wlan show profiles | Select-String "All User Profile\s+:\s+(.*)").Matches.Groups | Where-Object {$_.Value -notmatch "All User Profile*"} | Foreach {
+    $wlan=netsh wlan show profiles name=$_ key=clear
+      [pscustomobject][ordered]@{
+        'SSID' = ($wlan | Select-String "SSID Name\s+:\s+(.*)").Matches.Groups[1].Value
+          'Radio Type' = ($wlan | Select-String "Radio Type\s+:\s+(.*)").Matches.Groups[1].Value
+          'Authentication' = ($wlan | Select-String "Authentication\s+:\s+(.*)").Matches.Groups[1].Value
+          'Password' = ($wlan | Select-String "Key Content\s+:\s+(.*)").Matches.Groups[1].Value
+      }
+  } | Out-GridView -Title "Saved Wi-Fi passwords"
+}
+
 # https://stackoverflow.com/a/5501909/7136056
 function Reload-Profile {
     @(
