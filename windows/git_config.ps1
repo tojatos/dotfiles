417711782bf8fcc3c4061cbb599c38342bfcbf46
tojatos <tojatos@gmail.com>
2025-02-09 19:47:03 +0100

    Silence git push a bit

--- windows/git_config.ps1
+++ windows/git_config.ps1
@@ -26,6 +26,8 @@ git config --global push.autoSetupRemote true
 # Auto stash unstaged changes on `git pull`
 git config --global rebase.autoStash true
 
+# Make git say less. Silence.
+git config --global push.verbose false
+
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
-
